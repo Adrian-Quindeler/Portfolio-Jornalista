ecb6aea72b88e422eb5d7f84740adc5841a68d88
adrian-quindeler <adrianquindeler@gmail.com>
2025-07-01 22:02:55 -0300

    Novas páginas

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/videos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'videos_editados.html'));
+});
+
+app.get('/producoes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'producoes.html'));
+});
+
+app.get('/reportagens', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'reportagens.html'));
+});
+
+app.get('/fotografias', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'fotografias.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
