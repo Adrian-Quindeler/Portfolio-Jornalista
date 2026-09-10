7d725ee291cb02f00eecb25572217e82143cdb2a
adrian-quindeler <adrianquindeler@gmail.com>
2026-09-10 20:19:21 -0300

    Estilos dos 'Trabalhos'

--- Atualização/portfolio-jornalista/src/data/galerias.ts
+++ Atualização/portfolio-jornalista/src/data/galerias.ts
@@ -1,56 +1,49 @@
 export const galerias = {
     romariaMulheres: {
         title: "Romaria das Mulheres",
-        description:
-            "A Romaria das Mulheres envolve uma caminhada pelas ruas da cidade, com as mulheres se dirigindo para um local de celebração, como o Parque da Prainha em Vila Velha.",
+        description: "A Romaria das Mulheres envolve uma caminhada pelas ruas da cidade, com as mulheres se dirigindo para um local de celebração, como o Parque da Prainha em Vila Velha.",
         folder: "fotografias/romaria-mulheres",
         alt: "Fotografias da Romaria das Mulheres",
     },
 
     romariaConguistas: {
         title: "Romaria dos Conguistas",
-        description:
-            "A Romaria dos Conguistas é uma celebração de fé e cultura que faz parte da Festa da Penha em Vila Velha, Espírito Santo. Nela, as bandas de congo do estado sobem ao Convento da Penha para homenagear Nossa Senhora da Penha. Uma celebração que une músicas e danças.",
+        description: "A Romaria dos Conguistas é uma celebração de fé e cultura que faz parte da Festa da Penha em Vila Velha, Espírito Santo. Nela, as bandas de congo do estado sobem ao Convento da Penha para homenagear Nossa Senhora da Penha. Uma celebração que une músicas e danças.",
         folder: "fotografias/romaria-conquistas",
         alt: "Fotografias da Romaria dos Conguistas",
     },
 
     ilhaCaieiras: {
         title: "Ilha das Caieiras",
-        description:
-            "A Ilha das Caieiras, é um bairro da região da Grande São Pedro, em Vitória. Teve origem com o primeiro donatário da capitania do Espírito Santo, Vasco Fernandes Coutinho, durante a colonização do estado. A região também é conhecida por abrigar as Desfiadeiras de Siri e restaurantes de culinária típica. Do local os visitantes podem avistar diversos monumentos naturais.",
+        description: "A Ilha das Caieiras, é um bairro da região da Grande São Pedro, em Vitória. Teve origem com o primeiro donatário da capitania do Espírito Santo, Vasco Fernandes Coutinho, durante a colonização do estado. A região também é conhecida por abrigar as Desfiadeiras de Siri e restaurantes de culinária típica. Do local os visitantes podem avistar diversos monumentos naturais.",
         folder: "fotografias/ilha-caieiras",
         alt: "Fotografias da Ilha das Caieiras",
     },
 
     romariaCiclistas: {
         title: "Romaria dos Ciclistas",
-        description:
-            "A Romaria dos Ciclistas é um evento que combina fé e esporte, onde ciclistas pedalam em procissão para homenagear Nossa Senhora da Penha, a padroeira do Espírito Santo, dentro da programação da Festa da Penha em Vila Velha. O evento, que acontece anualmente na data da festa, reúne milhares de devotos em uma celebração de devoção, fé e gratidão.",
+        description: "A Romaria dos Ciclistas é um evento que combina fé e esporte, onde ciclistas pedalam em procissão para homenagear Nossa Senhora da Penha, a padroeira do Espírito Santo, dentro da programação da Festa da Penha em Vila Velha. O evento, que acontece anualmente na data da festa, reúne milhares de devotos em uma celebração de devoção, fé e gratidão.",
         folder: "fotografias/romaria-ciclistas",
         alt: "Fotografias da Romaria dos Ciclistas",
     },
 
     pequenoMC: {
         title: "Pequeno Movimento Cidade",
-        description:
-            "Festival do Movimento Cidade feito especialmente para as crianças com oficinas de arte, natureza, brincadeiras e imaginação soltas no ar. Em 2025, junto com o projeto de extensão da Faesa, foi realizada uma cobertura fotográfica durante os dias de oficinas.",
+        description: "Festival do Movimento Cidade feito especialmente para as crianças com oficinas de arte, natureza, brincadeiras e imaginação soltas no ar. Em 2025, junto com o projeto de extensão da Faesa, foi realizada uma cobertura fotográfica durante os dias de oficinas.",
         folder: "fotografias/pequeno-mc",
         alt: "Fotografias do Pequeno Movimento Cidade",
     },
 
     territorioBem: {
         title: "Território do Bem",
-        description:
-            "Visita técnica no bairro do Jaburu em Vitória, da disciplina Comunicação e Cidadania que busca ampliar o conhecimento da população sobre os direitos fundamentais. As fotos mostram a riqueza cultural e social do território produzida pelos próprios moradores.",
+        description: "Visita técnica no bairro do Jaburu em Vitória, da disciplina Comunicação e Cidadania que busca ampliar o conhecimento da população sobre os direitos fundamentais. As fotos mostram a riqueza cultural e social do território produzida pelos próprios moradores.",
         folder: "fotografias/territorio-bem",
         alt: "Fotografias do Território do Bem",
     },
 
     MCMulheres: {
         title: "MC Mulheres",
-        description:
-            "O MC Mulheres nasce do Movimento Cidade com o intuito de fortalecer a economia criativa e impulsionar a carreira de mulheres negras artistas da música e do audiovisual brasileiro. Em 2025, foi realizadas diversas oficinas. Junto com o projeto de extensão da Faesa, foi realizada uma cobertura fotográfica durante toda a semana do curso.",
+        description: "O MC Mulheres nasce do Movimento Cidade com o intuito de fortalecer a economia criativa e impulsionar a carreira de mulheres negras artistas da música e do audiovisual brasileiro. Em 2025, foi realizadas diversas oficinas. Junto com o projeto de extensão da Faesa, foi realizada uma cobertura fotográfica durante toda a semana do curso.",
         folder: "fotografias/mc-mulheres",
         alt: "Fotografias do MC Mulheres",
     },
