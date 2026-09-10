6bdb4739e1cb51f6bb6cc4d487fd875128a7f9ad
adrian-quindeler <adrianquindeler@gmail.com>
2026-09-10 19:45:18 -0300

    conexão com cloudnary

--- Atualização/portfolio-jornalista/src/data/fotografias.ts
+++ Atualização/portfolio-jornalista/src/data/fotografias.ts
@@ -1,37 +1,37 @@
 export const galleries = [
     {
         title: "Romaria das mulheres",
-        image: "/images/fotos/romaria-mulheres.jpg",
+        image: "fotografias/romaria-mulheres",
         href: "/trabalhos/fotografias/romariaMulheres",
     },
     {
         title: "Romaria dos conguistas",
-        image: "/images/fotos/romaria-conguistas.jpg",
+        image: "fotografias/romaria-conguistas",
         href: "/trabalhos/fotografias/romariaConguistas",
     },
     {
         title: "Ilha das Caieiras",
-        image: "/images/fotos/ilha-caieiras.jpg",
+        image: "fotografias/ilha-caieiras",
         href: "/trabalhos/fotografias/ilhaCaieiras",
     },
     {
         title: "Romaria dos ciclistas",
-        image: "/images/fotos/romaria-ciclistas.jpg",
+        image: "fotografias/romaria-ciclistas",
         href: "/trabalhos/fotografias/romariaCiclistas",
     },
     {
         title: "Pequeno Movimento Cidade",
-        image: "/images/fotos/pequeno-mc.jpg",
+        image: "fotografias/pequeno-mc",
         href: "/trabalhos/fotografias/pequenoMC",
     },
     {
         title: "Território do Bem",
-        image: "/images/fotos/territorio-bem.jpg",
+        image: "fotografias/territorio-bem",
         href: "/trabalhos/fotografias/territorioBem",
     },
     {
         title: "MC Mulheres",
-        image: "/images/fotos/mc-mulheres.jpg",
+        image: "fotografias/mc-mulheres",
         href: "/trabalhos/fotografias/MCMulheres",
     },
 ];
--- Atualização/portfolio-jornalista/src/data/reportagens.ts
+++ Atualização/portfolio-jornalista/src/data/reportagens.ts
@@ -3,7 +3,7 @@ export const reports = [
         title: "Empreender com dança",
         description:
             "As dificuldades para empreender e ter sucesso com a arte da dança no Espírito Santo",
-        image: "/images/reportagens/danca.jpg",
+        image: "reportagens/danca",
         href: "https://lacosfaesa.com.br/2024/05/22/as-dificuldades-para-empreender-e-ter-sucesso-com-a-arte-da-danca-no-espirito-santo/",
         linkLabel: "Ver reportagem",
     },
@@ -11,14 +11,14 @@ export const reports = [
         title: "Tatuagens de alerta",
         description:
             "Tatuagens de alerta: o que são e por que estão cada vez mais comuns",
-        image: "/images/reportagens/tatuagens.jpg",
+        image: "reportagens/tatuagens",
         href: "https://www.agazeta.com.br/redacao-integrada/tatuagens-de-alerta-o-que-sao-e-por-que-estao-cada-vez-mais-comuns-0124",
         linkLabel: "Ver reportagem",
     },
     {
         title: "Cooperativa mirim",
         description: "Cooperativa mirim no ES ensina alunos a comandar negócio",
-        image: "/images/reportagens/cooperativa.jpg",
+        image: "reportagens/cooperativa",
         href: "https://www.agazeta.com.br/es/economia/cooperativa-mirim-no-es-ensina-alunos-a-comandar-negocios-1024",
         linkLabel: "Ver reportagem",
     },
@@ -26,7 +26,7 @@ export const reports = [
         title: "Mãe enfermeira",
         description:
             "Mãe se forma na área de Saúde para cuidar de filho com doença rara",
-        image: "/images/reportagens/enfermeira.jpg",
+        image: "reportagens/enfermeira",
         href: "https://www.agazeta.com.br/es/cotidiano/mae-se-forma-na-area-de-saude-para-cuidar-de-filho-com-doenca-rara-1124",
         linkLabel: "Ver reportagem",
     },
@@ -34,7 +34,7 @@ export const reports = [
         title: "Boneco de neve",
         description:
             "Baixas temperaturas congelam cachoeira, e turistas fazem 'bonequinho de neve' no ES",
-        image: "/images/reportagens/neve.jpg",
+        image: "reportagens/neve",
         href: "https://g1.globo.com/es/espirito-santo/noticia/2025/06/15/baixas-temperaturas-congelam-cachoeira-e-turistas-fazem-bonequinho-de-neve-no-es.ghtml",
         linkLabel: "Ver reportagem",
     },
